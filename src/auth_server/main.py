00aac3957ff483a34b3011eeecc680c49b2467de
MK <qscvdefb@gmail.com>
2025-02-05 16:45:56 +0700

    up

--- src/auth_server/main.py
+++ src/auth_server/main.py
@@ -1,13 +1,13 @@
 import threading
 
 try:
-    from .view import run
+    # from .view import run
 
-    # from .controller.token_receive import host
-    # from .service.sso import open_sso
+    from .controller.token_receive import host
+    from .service.sso import open_sso
 
-    # threading.Thread(target=open_sso).start()
-    # host()
+    threading.Thread(target=open_sso).start()
+    host()
 
 except Exception as e:
     print("Lỗi xác thực người dùng")
--- src/auth_server/view/run.py
+++ src/auth_server/view/run.py
@@ -1,16 +1,29 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtGui import QScreen
 
-# Khởi tạo ứng dụng
 app = QApplication(sys.argv)
 
 # Tạo cửa sổ chính
 window = QWidget()
 window.setWindowTitle("Giao diện PyQt5")
-window.setGeometry(100, 100, 400, 300)  # X, Y, Width, Height
+
+# Lấy thông tin màn hình
+screen = app.primaryScreen()
+screen_geometry = screen.geometry()
+
+# Kích thước cửa sổ
+window_width = 400
+window_height = 300
+
+# Tính toán vị trí cửa sổ sao cho nó ở giữa màn hình
+x = (screen_geometry.width() - window_width) // 2
+y = (screen_geometry.height() - window_height) // 3
+
+# Đặt vị trí và kích thước cửa sổ
+window.setGeometry(x, y, window_width, window_height)
 
 # Hiển thị cửa sổ
 window.show()
 
-# Chạy vòng lặp ứng dụng
 sys.exit(app.exec_())
